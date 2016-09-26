f5261b67afab88bd8732d56332aa821d0af73f1f
odrescher <odsoftware@gmx.de>
2016-09-26 15:04:01 +0200

    fixes for new server

---
+++ etc/52.28.112.176.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'application'	=> array(
+		'useLayout'		=> true,
+		'defaultLayoutTemplate' => 'layout/layout',
+		'loginttl' => 600, //Nach 10 Min (60 Sekunden * 10) erfolgt automatischer logout
+		'baseUrl' => 'http://52.28.112.176/'
+	),
+	'user'	=> false,
+	'debug' => false
+);
